6b855d20590b162c08ebf9aa342ffc540db3930d
zilch <wlgys8@gmail.com>
2020-06-22 00:04:41 +0800

    add  UpdateAwaiter

---
+++ Runtime/CompilerServices/UpdateAwaiter.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.CompilerServices;
+using System;
+
+
+namespace MS.Async.CompilerServices{
+    public struct UpdateAwaiter : ICriticalNotifyCompletion
+    {
+
+        public void GetResult(){
+            
+        }
+        public bool IsCompleted{
+            get{
+                return false;
+            }
+        }
+
+        public void OnCompleted (Action continuation){
+            UnsafeOnCompleted(continuation);
+        }
+
+        public void UnsafeOnCompleted (Action continuation){
+            UnityLoops.ScheduleUpdate(continuation);
+        }
+    }
+}
