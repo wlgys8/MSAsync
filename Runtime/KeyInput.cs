2f861120a4e81276a343da1544575a21d6b3a488
zilch <wlgys8@gmail.com>
2020-06-22 00:05:01 +0800

    add KeyInput

---
+++ Runtime/KeyInput.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+
+namespace MS.Async{
+    using CompilerServices;
+    public struct KeyInput
+    {
+       
+        private KeyCode _keycode;
+        private KeyInutType _type;
+        public KeyInput(KeyCode code,KeyInutType type){
+            _keycode = code;
+            _type = type;
+        }
+
+        public KeyInputAwait GetAwaiter(){
+            return new KeyInputAwait(_keycode,_type);
+        }
+    }
+
+
+    public enum KeyInutType{
+        Down,
+        
+        Up,
+        Any,
+    }
+}
